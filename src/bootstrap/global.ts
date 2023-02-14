15d76e10e67eea0c239d5335284e82f55af0ab7c
Huan <ngquhuanbl@gmail.com>
2023-02-14 21:37:06 +0700

    feat: Turn off verify result

--- src/bootstrap/global.ts
+++ src/bootstrap/global.ts
@@ -1,5 +1,5 @@
 import { getConvId } from "../helpers/shared/generate-data";
 
-window.PARTITION_MODE = false;
+window.PARTITION_MODE = true;
 window.SELECTED_PARTITION_KEY = getConvId(0);
-window.VERIFY_MODE_ON = true;
+window.VERIFY_MODE_ON = false;
